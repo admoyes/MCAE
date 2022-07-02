becd5b092487760c7038f1a6e7923cb8cf9e03dc
admoyes <admoyes@glass.home>
2022-07-02 17:54:49 +0100

    mlflow run names

--- src/train.py
+++ src/train.py
@@ -4,19 +4,42 @@ from torch.nn import functional as F
 from torch.utils.data import DataLoader
 from datasets import TripletPatchDataset
 from models.mcae import train_mcae
+from models.stanosa import train_stanosa
 import mlflow
 
+# define stanosa dataset and dataloader
+dataset = TripletPatchDataset("/data", 8, False)
 
-# MCAE training
-dataset = TripletPatchDataset(
-    "/data",
-    8
-)
 dataloader = DataLoader(
     dataset,
     num_workers=6,
     shuffle=True,
     batch_size=128,
 )
-with mlflow.start_run(experiment_id="mcae"):
-    mcae_model = train_mcae(dataloader)
+
+# train stanosa models
+with mlflow.start_run(run_name="stanosa_a", description="StaNoSA Training - Domain A"):
+    mlflow.set_tag("mlflow.runName", "stanosa_a")
+    train_stanosa(dataloader, 0)
+
+with mlflow.start_run(run_name="stanosa_b", description="StaNoSA Training - Domain B"):
+    mlflow.set_tag("mlflow.runName", "stanosa_b")
+    train_stanosa(dataloader, 1)
+
+with mlflow.start_run(run_name="stanosa_c", description="StaNoSA Training = Domain C"):
+    mlflow.set_tag("mlflow.runName", "stanosa_c")
+    train_stanosa(dataloader, 2)
+
+# define the MCAE dataset and dataloader
+dataset = TripletPatchDataset("/data", 8, True)
+
+dataloader = DataLoader(
+    dataset,
+    num_workers=6,
+    shuffle=True,
+    batch_size=128,
+)
+
+# train MCAE model
+with mlflow.start_run(run_name="MCAE", description="MCAE Training"):
+    train_mcae(dataloader)
