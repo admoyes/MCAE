7a8fbc8fc7bdae754e926945e89288a35849da7e
admoyes <admoyes@glass.home>
2022-11-25 22:38:57 +0000

    clean up train runfile

--- src/run_train.sh
+++ src/run_train.sh
@@ -24,90 +24,12 @@ run_training() {
 
 
 #################################################################
-# STANOSA Training
-#################################################################
-
-# train StaNoSA with domain A (0th index)
-run_training stanosa_a stanosa 0
-#mlflow run . \
-#	-e main \
-#	--experiment-name training \
-#	--run-name stanosa_a \
-#	--docker-args gpus=all \
-#	--docker-args shm-size=2G \
-#	-P model-type=stanosa \
-#	-P stanosa-domain-index=0 \
-#	-P epochs=50
-	
-
-# train StaNoSA with domain B (1st index)
-run_training stanosa_b stanosa 1
-#mlflow run . \
-#	-e main \
-#	--experiment-name training \
-#	--run-name stanosa_b \
-#	--docker-args gpus=all \
-#	--docker-args shm-size=2G \
-#	-P model-type=stanosa \
-#	-P stanosa-domain-index=1 \
-#	-P epochs=50
-
-
-# train StaNoSA with domain C (2nd index)
-run_training stanosa_c stanosa 2
-#mlflow run . \
-#	-e main \
-#	--experiment-name training \
-#	--run-name stanosa_c \
-#	--docker-args gpus=all \
-#	--docker-args shm-size=2G \
-#	-P model-type=stanosa \
-#	-P stanosa-domain-index=2 \
-#	-P epochs=50
-
-#################################################################
 # DCAE Training
 #################################################################
 
 run_training dcae_ab dcae 0 1
-#mlflow run . \
-#	-e main \
-#	--experiment-name training \
-#	--run-name dcae_ab \
-#	--docker-args gpus=all \
-#	--docker-args shm-size=2G \
-#	-P model-type=dcae \
-#	-P epochs=50 \
-#	-P dcae-domain-index-1=0 \
-#	-P dcae-domain-index-2=1 \
-#
-#
 run_training dcae_ac dcae 0 2
-#mlflow run . \
-#	-e main \
-#	--experiment-name training \
-#	--run-name dcae_ac \
-#	--docker-args gpus=all \
-#	--docker-args shm-size=2G \
-#	-P model-type=dcae \
-#	-P epochs=50 \
-#	-P dcae-domain-index-1=0 \
-#	-P dcae-domain-index-2=2 \
-#
-#
 run_training dcae_bc dcae 1 2
-#mlflow run . \
-#	-e main \
-#	--experiment-name training \
-#	--run-name dcae_bc \
-#	--docker-args gpus=all \
-#	--docker-args shm-size=2G \
-#	-P model-type=dcae \
-#	-P epochs=50 \
-#	-P dcae-domain-index-1=1 \
-#	-P dcae-domain-index-2=2 \
-
-
 
 
 #################################################################
@@ -116,12 +38,17 @@ run_training dcae_bc dcae 1 2
 
 # train MCAE
 run_training mcae mcae 
-#mlflow run . \
-#	-e main \
-#	--experiment-name training \
-#	--run-name mcae \
-#	--docker-args gpus=all \
-#	--docker-args shm-size=2G \
-#	-P model-type=mcae \
-#	-P epochs=50
 
+
+#################################################################
+# STANOSA Training
+#################################################################
+
+# train StaNoSA with domain A (0th index)
+run_training stanosa_a stanosa 0
+
+# train StaNoSA with domain B (1st index)
+run_training stanosa_b stanosa 1
+
+# train StaNoSA with domain C (2nd index)
+run_training stanosa_c stanosa 2
